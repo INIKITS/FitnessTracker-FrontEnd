e020e0c7d6f954404198e1a9051d796ea57b8a87
Dakota Solis <dakota.h.solis@gmail.com>
2022-09-01 02:05:11 -0500

    added basic header and routes

---
+++ src/components/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Header(props) {
+  const { isLoggedIn } = props;
+
+  console.log("isLoggedIn", isLoggedIn);
+  return (
+    <div id="header">
+      <h1 id="header-text">Fitness Tracker</h1>
+      <nav>
+        <ul id="nav-list">
+          <li>
+            <NavLink
+              to="/"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline" : "none",
+              })}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-text">
+            <NavLink
+              to="activities"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline" : "none",
+              })}
+            >
+              Activities
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="routines"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline" : "none",
+              })}
+            >
+              Routines
+            </NavLink>
+          </li>
+
+          <li>
+            {isLoggedIn ? (
+              <NavLink
+                to="/"
+                style={({ isActive }) => ({
+                  textDecoration: isActive ? "underline" : "none",
+                })}
+                //   onClick={handleLogout}
+              >
+                Log Out
+              </NavLink>
+            ) : (
+              <li>
+                {" "}
+                <NavLink
+                  to="/"
+                  style={({ isActive }) => ({
+                    textDecoration: isActive ? "underline" : "none",
+                  })}
+                >
+                  {" "}
+                  Log in
+                </NavLink>
+              </li>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/MyRoutines.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function MyRoutines(props) {
+    return (
+
+        <h1>My Routines</h1>
+    )
+}
+
+export default MyRoutines;
