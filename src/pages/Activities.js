04d18d0995cb435395e5ff4c104415c04da6de8b
AshCheeks <adelarosa2013@gmail.com>
2022-09-01 22:25:14 -0400

    worked on homepage

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import {activities} from '../api';
 
 function Activities(props) {
-  return <h1>Activities</h1>;
+  return (
+  <div> 
+    <h1>{activities.name}</h1>
+    </div>
+  )
 }
+
+
+
 
 export default Activities;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import '../styles/Home.scss';
 import HomeImage from '../img/pexels-ketut-subiyanto-4436349.jpg';
 
 function Home(props) {
+
   return (
     <div id='home-wrapper'>
       <p className='title1'>TRACK YOUR</p>
