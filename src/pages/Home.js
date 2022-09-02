3f88ca8739ff793ba4c89cb6fb95381a9bcbffe6
AshCheeks <adelarosa2013@gmail.com>
2022-09-01 22:24:01 -0400

    work on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { LoginModal } from '../components/index';
+import '../styles/Home.scss';
+import HomeImage from '../img/pexels-ketut-subiyanto-4436349.jpg';
 
 function Home(props) {
   return (
-    <>
-      <h1>TRACK YOUR</h1>
-      <h1>FITNESS</h1>
-      <LoginModal />
-    </>
+    <div id='home-wrapper'>
+      <p className='title1'>TRACK YOUR</p>
+      <p className='title2'>FITNESS</p>
+      {/* <LoginModal /> */}
+    </div>
   );
 }
 
