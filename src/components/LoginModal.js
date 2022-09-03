e81b2bb0f660b8c113a83b33f7f148d9d5eeaa56
AshCheeks <adelarosa2013@gmail.com>
2022-09-03 14:10:10 -0400

    fixed the header styling

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { login } from "../api";
-import "../styles/Login-Register.scss";
-import { FaUser } from "react-icons/fa";
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../api';
+import '../styles/Login-Register.scss';
+import { FaUser } from 'react-icons/fa';
 
 function LoginModal(props) {
   const navigate = useNavigate();
-  const [errorMessage, setErrorMessage] = React.useState("");
+  const [errorMessage, setErrorMessage] = React.useState('');
   const [callSuccess, setCallSuccess] = React.useState(true);
   const [modal, setModal] = React.useState(false);
   const {
@@ -31,42 +31,42 @@ function LoginModal(props) {
       setCallSuccess
     );
 
-    navigate("/");
+    navigate('/');
   };
 
   return (
     <>
-      <div id="modal">
-        <div id="new-post-form">
+      <div id='modal'>
+        <div id='new-post-form'>
           <form onSubmit={handleSubmit}>
             <h2>Login</h2>
-            <div className="form-group">
+            <div className='form-group'>
               <input
-                type="text"
-                id="login-title"
-                name="login"
-                placeholder="Enter username"
+                type='text'
+                id='login-title'
+                name='login'
+                placeholder='Enter username'
                 required={true}
                 onChange={(e) => {
                   setUsername(e.target.value);
                 }}
               ></input>
             </div>
-            <div className="form-group">
+            <div className='form-group'>
               <input
-                type="password"
-                id="login-password"
-                name="password"
-                placeholder="Enter password"
+                type='password'
+                id='login-password'
+                name='password'
+                placeholder='Enter password'
                 required={true}
                 onChange={(e) => {
                   setPassword(e.target.value);
                 }}
               ></input>
             </div>
-            {callSuccess ? null : <p id="register-error">{errorMessage}</p>}
-            <button type="submit">SUBMIT</button>
-            <Link to="/register">Need an account? Click here to register</Link>
+            {callSuccess ? null : <p id='register-error'>{errorMessage}</p>}
+            <button type='submit'>SUBMIT</button>
+            <Link to='/register'>Need an account? Click here to register</Link>
           </form>
         </div>
       </div>
