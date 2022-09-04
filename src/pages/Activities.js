12cad58b6fbf5b5b45858714170159ad3c8ddaac
Kiet Hoang <kiethoang123@yahoo.com>
2022-09-04 16:31:32 -0500

    activityform

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { getActivities } from "../api";
+import { CreateActivityForm } from "../components";
 import "../styles/Activities.scss";
 
 function Activities(props) {
@@ -13,10 +14,14 @@ function Activities(props) {
   }, [activities]);
   return (
     <div className="activity-wrapper">
+      <CreateActivityForm
+            errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
+        userToken={userToken}
+        /> 
       {activities &&
         activities.map((activity) => {
-          return (
-            <div className="activity-container" key={activity.id}>
+          return (<div className="activity-container" key={activity.id}>
               <p>Name: {activity.name}</p>
               <p>Description: {activity.description}</p>
             </div>
